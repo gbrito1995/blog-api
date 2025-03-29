4099e24cf04fea35f5c5bf3fd6e9c87acc8f01e2
Gabriel <gabrieldsb95@gmail.com>
2025-03-29 12:55:32 -0300

    corretion of token validation

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -5,7 +5,11 @@ module.exports = (req, res, next) => {
   let token = req.headers.token;  
   if (!token) return res.status(401).send("Access denied!");
 
-  let isValid = jwt.verify(token, `${process.env.SECRET_PASS}`);  
-  if (isValid) next();
-  else return res.status(400).send(isValid);
+  jwt.verify(token, `${process.env.SECRET_PASS}`, (err, decode) => {
+    
+    if (err) res.status(401).send("Token is not valid.");
+    else next();
+    
+  });  
+  
 }
